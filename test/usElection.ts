7ac622249146a9aa946068a084f656c75e597adb
DanielDimitrov5 <danieldimitrov2304@gmail.com>
2023-05-20 12:27:42 +0300

    100% code coverage

--- test/usElection.ts
+++ test/usElection.ts
@@ -4,66 +4,107 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 
 describe("USElection", function () {
-  let usElectionFactory;
-  let usElection: USElection;
+    let usElectionFactory;
+    let usElection: USElection;
 
-  before(async () => {
-    usElectionFactory = await ethers.getContractFactory("USElection");
+    let accounts: any;
 
-    usElection = await usElectionFactory.deploy();
+    before(async () => {
+        usElectionFactory = await ethers.getContractFactory("USElection");
 
-    await usElection.deployed();
-  });
+        usElection = await usElectionFactory.deploy();
 
-  it("Should return the current leader before submit any election results", async function () {
-    expect(await usElection.currentLeader()).to.equal(0); // NOBODY
-  });
+        await usElection.deployed();
 
-  it("Should return the election status", async function () {
-    expect(await usElection.electionEnded()).to.equal(false); // Not Ended
-  });
+        accounts = await ethers.getSigners();
+    });
 
-  it("Should submit state results and get current leader", async function () {
-    const stateResults = ["California", 1000, 900, 32];
+    it("Should return the current leader before submit any election results", async function () {
+        expect(await usElection.currentLeader()).to.equal(0); // NOBODY
+    });
 
-    const submitStateResultsTx = await usElection.submitStateResult(
-      stateResults
-    );
+    it("Should return the election status", async function () {
+        expect(await usElection.electionEnded()).to.equal(false); // Not Ended
+    });
 
-    await submitStateResultsTx.wait();
+    it("Should submit state results and get current leader", async function () {
+        const stateResults: any = ["California", 1000, 900, 32];
 
-    expect(await usElection.currentLeader()).to.equal(1); // BIDEN
-  });
+        const submitStateResultsTx = await usElection.submitStateResult(
+            stateResults
+        );
+        await submitStateResultsTx.wait();
 
-  it("Should throw when try to submit already submitted state results", async function () {
-    const stateResults = ["California", 1000, 900, 32];
+        //New
+        await expect(submitStateResultsTx).to.emit(usElection, "LogStateResult").withArgs(1, 32, "California"); // BIDEN
 
-    expect(usElection.submitStateResult(stateResults)).to.be.revertedWith(
-      "This state result was already submitted!"
-    );
-  });
+        expect(await usElection.currentLeader()).to.equal(1); // BIDEN
+    });
 
-  it("Should submit state results and get current leader", async function () {
-    const stateResults = ["Ohaio", 800, 1200, 33];
+    it("Should throw when try to submit already submitted state results", async function () {
+        const stateResults: any = ["California", 1000, 900, 32];
 
-    const submitStateResultsTx = await usElection.submitStateResult(
-      stateResults
-    );
+        //Fixed
+        await expect(usElection.submitStateResult(stateResults)).to.be.revertedWith(
+            "This state result was already submitted!"
+        );
+    });
 
-    await submitStateResultsTx.wait();
+    it("Should submit state results and get current leader", async function () {
+        const stateResults: any = ["Ohaio", 800, 1200, 33];
 
-    expect(await usElection.currentLeader()).to.equal(2); // TRUMP
-  });
+        const submitStateResultsTx = await usElection.submitStateResult(
+            stateResults
+        );
 
-  it("Should end the elections, get the leader and election status", async function () {
-    const endElectionTx = await usElection.endElection();
+        await submitStateResultsTx.wait();
 
-    await endElectionTx.wait();
+        expect(await usElection.currentLeader()).to.equal(2); // TRUMP
+    });
 
-    expect(await usElection.currentLeader()).to.equal(2); // TRUMP
+    //New
+    it("Should throw if state seats are zero", async function () {
+        const stateResults: any = ["Ohaio", 800, 1200, 0];
 
-    expect(await usElection.electionEnded()).to.equal(true); // Ended
-  });
+        await expect(usElection.submitStateResult(stateResults)).to.be.revertedWith("States must have at least 1 seat");
+    });
 
-  //TODO: ADD YOUR TESTS
+    //New
+    it("Should throw if state results are equal", async function () {
+        const stateResults: any = ["Ohaio", 1000, 1000, 33];
+
+        await expect(usElection.submitStateResult(stateResults)).to.be.revertedWith("There cannot be a tie");
+    });
+
+    it("Should end the elections, get the leader and election status", async function () {
+        const endElectionTx = await usElection.endElection();
+        await endElectionTx.wait();
+
+        //New
+        await expect(endElectionTx).to.emit(usElection, "LogElectionEnded").withArgs(2); // TRUMP
+
+        expect(await usElection.currentLeader()).to.equal(2); // TRUMP
+        expect(await usElection.electionEnded()).to.equal(true); // Ended
+    });
+
+    //TODO: ADD YOUR TESTS
+    it("Should throw if not owner is trying to submit state results", async function () {
+        const stateResults: any = ["Ohaio", 800, 1200, 33];
+
+        await expect(usElection.connect(accounts[1]).submitStateResult(stateResults)).to.be.revertedWith("Not invoked by the owner");
+    });
+
+    it("Should throw if try to submit state results when election has ended", async function () {
+        const stateResults: any = ["Ohaio", 800, 1200, 33];
+
+        await expect(usElection.submitStateResult(stateResults)).to.be.revertedWith("The election has ended already");
+    });
+
+    it("Should throw if try to end the election when it has ended already", async function () {
+        await expect(usElection.endElection()).to.be.revertedWith("The election has ended already");
+    });
+
+    it("Should throw if not owner try to end the election", async function () {
+        await expect(usElection.connect(accounts[1]).endElection()).to.be.revertedWith("Not invoked by the owner");
+    });
 });
